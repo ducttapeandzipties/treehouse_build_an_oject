bcda21336fbfbba4195dde91e8e641c3c8fac823
Dan Wagner <dan.nmi.wagner@gmail.com>
2016-09-12 18:50:41 -0400

    created an array of objects

---
+++ js/students.js
@@ -0,0 +1,32 @@
+var studentData = [
+  {
+    name: "Dan",
+    track: "js",
+    achievements: "10",
+    points: "50"
+  },
+  {
+    name: "Jane",
+    track: "IOS",
+    achievements: "20",
+    points: "55"
+  },
+  {
+    name: "Bill",
+    track: "CSS",
+    achievements: "4",
+    points: "40"
+  },
+  {
+    name: "Nancy",
+    track: "HTML",
+    achievements: "3",
+    points: "30"
+  },
+  {
+    name: "Jordan",
+    track: "C#",
+    achievements: "15",
+    points: "75"
+  },
+];
